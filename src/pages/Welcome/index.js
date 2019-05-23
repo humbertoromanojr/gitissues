17b7b6140ec0088817a3e60788f31a891d341301
HJr Dev <astronomi@gmail.com>
2019-05-23 17:19:03 -0300

    index.js StatusBar

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {
-  View, Text, TextInput, TouchableOpacity,
+  View, Text, TextInput, TouchableOpacity, StatusBar,
 } from 'react-native';
 
 import styles from './styles';
 
 const Welcome = () => (
   <View style={styles.container}>
+    <StatusBar barStyle="light-content" backgroundColor="#212121" />
+
     <Text style={styles.title}>Bem-vindo</Text>
     <Text style={styles.text}>Para continuar, informe seu Github!</Text>
 
