96f231b7dcefbcc4ded066d5efa1fdad00d7857b
HJr Dev <astronomi@gmail.com>
2019-05-23 22:24:23 -0300

    config and created API

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,30 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   View, Text, TextInput, TouchableOpacity, StatusBar,
 } from 'react-native';
 
 import styles from './styles';
 
-const Welcome = () => (
-  <View style={styles.container}>
-    <StatusBar barStyle="light-content" backgroundColor="#212121" />
+export default class Welcome extends Component {
+  state = {
+    username: '',
+  };
 
-    <Text style={styles.title}>Bem-vindo</Text>
-    <Text style={styles.text}>Para continuar, informe seu Github!</Text>
+  signIn = () => {};
 
-    <View style={styles.form}>
-      <TextInput
-        style={styles.input}
-        autoCapitalize="none"
-        autoCorrect={false}
-        placeholder="Digite seu usuário"
-        underlineColorAndroid="transparent"
-      />
-      <TouchableOpacity style={styles.button} onPress={() => {}}>
-        <Text style={styles.buttonText}>Prosseguir</Text>
-      </TouchableOpacity>
-    </View>
-  </View>
-);
+  render() {
+    const { username } = this.state;
 
-export default Welcome;
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content" backgroundColor="#212121" />
+
+        <Text style={styles.title}>Bem-vindo</Text>
+        <Text style={styles.text}>Para continuar, informe seu Github!</Text>
+
+        <View style={styles.form}>
+          <TextInput
+            style={styles.input}
+            autoCapitalize="none"
+            autoCorrect={false}
+            placeholder="Digite seu usuário"
+            underlineColorAndroid="transparent"
+            value={username}
+            onChangeText={text => this.setState({ username: text })}
+          />
+          <TouchableOpacity style={styles.button} onPress={this.signIn}>
+            <Text style={styles.buttonText}>Prosseguir</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+}
